06e19b8cd454c06a976d81f2a9eb73a872e8fe8d
Francisco Bórquez <fborquez@outlook.com>
2023-02-12 17:33:28 -0300

    feat: setup configuration

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='news-analyzer',
+      version='1.0',
+      description='News Analyzer',
+      author='Francisco Bórquez',
+      author_email='fborquez@outlook.com',
+      url='',
+      packages=[],
+     )
